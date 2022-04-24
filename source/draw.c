b5620f4a671c5a02bfe1dfb1f2759532db8917b9
Joshua Robertson <joshrobertson774@gmail.com>
2022-04-24 02:47:48 +0100

    Made drawing functions work with numbers.

--- source/draw.c
+++ source/draw.c
@@ -15,22 +15,22 @@ static Color get_lua_color_arg(lua_State* lua, int offs)
     switch(comps)
     {
         case 1: // RRR1
-            col.r = CLAMP(luaL_checkinteger(lua, offs+0),0,255);
-            col.g = CLAMP(luaL_checkinteger(lua, offs+0),0,255);
-            col.b = CLAMP(luaL_checkinteger(lua, offs+0),0,255);
+            col.r = CLAMP(luaL_checknumber(lua, offs+0),0,255);
+            col.g = CLAMP(luaL_checknumber(lua, offs+0),0,255);
+            col.b = CLAMP(luaL_checknumber(lua, offs+0),0,255);
             col.a = 255;
         break;
         case 3: // RGB1
-            col.r = CLAMP(luaL_checkinteger(lua, offs+0),0,255);
-            col.g = CLAMP(luaL_checkinteger(lua, offs+1),0,255);
-            col.b = CLAMP(luaL_checkinteger(lua, offs+2),0,255);
+            col.r = CLAMP(luaL_checknumber(lua, offs+0),0,255);
+            col.g = CLAMP(luaL_checknumber(lua, offs+1),0,255);
+            col.b = CLAMP(luaL_checknumber(lua, offs+2),0,255);
             col.a = 255;
         break;
         case 4: // RGBA
-            col.r = CLAMP(luaL_checkinteger(lua, offs+0),0,255);
-            col.g = CLAMP(luaL_checkinteger(lua, offs+1),0,255);
-            col.b = CLAMP(luaL_checkinteger(lua, offs+2),0,255);
-            col.a = CLAMP(luaL_checkinteger(lua, offs+3),0,255);
+            col.r = CLAMP(luaL_checknumber(lua, offs+0),0,255);
+            col.g = CLAMP(luaL_checknumber(lua, offs+1),0,255);
+            col.b = CLAMP(luaL_checknumber(lua, offs+2),0,255);
+            col.a = CLAMP(luaL_checknumber(lua, offs+3),0,255);
         break;
     }
     return col;
@@ -93,8 +93,8 @@ LUA_FUNCTION(cls)
 
 LUA_FUNCTION(px)
 {
-    int x = luaL_checkinteger(lua, 1);
-    int y = luaL_checkinteger(lua, 2);
+    int x = luaL_checknumber(lua, 1);
+    int y = luaL_checknumber(lua, 2);
     Color c = get_lua_color_arg(lua, 3);
       set_pixel(x,y,c);
     return 0;
@@ -102,10 +102,10 @@ LUA_FUNCTION(px)
 
 LUA_FUNCTION(line)
 {
-    int  x0 = luaL_checkinteger(lua, 1);
-    int  y0 = luaL_checkinteger(lua, 2);
-    int  x1 = luaL_checkinteger(lua, 3);
-    int  y1 = luaL_checkinteger(lua, 4);
+    int  x0 = luaL_checknumber(lua, 1);
+    int  y0 = luaL_checknumber(lua, 2);
+    int  x1 = luaL_checknumber(lua, 3);
+    int  y1 = luaL_checknumber(lua, 4);
     Color c = get_lua_color_arg(lua, 5);
 
     draw_line(x0,y0,x1,y1, c);
@@ -115,11 +115,11 @@ LUA_FUNCTION(line)
 
 LUA_FUNCTION(rect)
 {
-    int mode = luaL_checkinteger(lua, 1);
-    int    x = luaL_checkinteger(lua, 2);
-    int    y = luaL_checkinteger(lua, 3);
-    int    w = luaL_checkinteger(lua, 4);
-    int    h = luaL_checkinteger(lua, 5);
+    int mode = luaL_checknumber(lua, 1);
+    int    x = luaL_checknumber(lua, 2);
+    int    y = luaL_checknumber(lua, 3);
+    int    w = luaL_checknumber(lua, 4);
+    int    h = luaL_checknumber(lua, 5);
     Color  c = get_lua_color_arg(lua, 6);
 
     // Don't even bother rendering if we're offscreen.
@@ -160,11 +160,11 @@ LUA_FUNCTION(rect)
 
 LUA_FUNCTION(circ)
 {
-    int mode = luaL_checkinteger(lua, 1);
-    int    x = luaL_checkinteger(lua, 2);
-    int    y = luaL_checkinteger(lua, 3);
-    int    r = luaL_checkinteger(lua, 4);
-    int    t = luaL_checkinteger(lua, 5);
+    int mode = luaL_checknumber(lua, 1);
+    int    x = luaL_checknumber(lua, 2);
+    int    y = luaL_checknumber(lua, 3);
+    int    r = luaL_checknumber(lua, 4);
+    int    t = luaL_checknumber(lua, 5);
     Color  c = get_lua_color_arg(lua, 6);
 
     // If the user wants fill mode we just set the thickness to the radius and that will make a filled circle.
@@ -230,8 +230,8 @@ LUA_FUNCTION(circ)
 
 LUA_FUNCTION(get)
 {
-    int x = luaL_checkinteger(lua, 1);
-    int y = luaL_checkinteger(lua, 2);
+    int x = luaL_checknumber(lua, 1);
+    int y = luaL_checknumber(lua, 2);
 
     Color c = {0};
     if(x >= 0 && x < SCREEN_W && y >= 0 && y < SCREEN_H)
