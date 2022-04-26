ef104dd02d5b16b0002ad58312ded80d5da0ea1c
Joshua Robertson <joshrobertson774@gmail.com>
2022-04-26 23:05:06 +0100

    Some reorganization + build improvements + app now builds to web directory.

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @ECHO off
 
-IF NOT EXIST bin MKDIR bin
+IF NOT EXIST web\app MKDIR web\app
 
 IF "%~1"=="clean" GOTO clean_dir
 IF "%~1"=="server" GOTO run_server
@@ -10,34 +10,27 @@ GOTO build_app
 
 :clean_dir
 ECHO cleaning build...
-IF EXIST "bin" DEL bin\*.*?
+IF EXIST "web\app" DEL web\app\*.*?
 GOTO end
 
 :run_server
 ECHO starting server...
-PUSHD bin
+PUSHD web
 START python -m http.server
 POPD
 GOTO end
 
 :run_setup
 ECHO setting up emsdk...
-PUSHD app\emsdk
-CALL emsdk install latest
-CALL emsdk activate latest
-CALL emsdk_env.bat
-POPD
+CALL app\emsdk emsdk install latest
+CALL app\emsdk activate latest
+CALL app\emsdk\emsdk_env.bat
 GOTO end
 
 :build_app
 ECHO building application...
-PUSHD bin
-CALL emcc -s WASM=1 -s USE_SDL=2 -I ..\app\lua ..\app\pixapp.c -o pixtoy.js
-COPY ..\web\*.html *.html
-COPY ..\web\*.js *.js
-COPY ..\web\ace\*.js *.js
-COPY ..\web\*.css *.css
-POPD
+IF NOT EXIST web\app MKDIR web\app
+CALL emcc -s WASM=1 -s USE_SDL=2 -I app\lua app\pixapp.c -o web\app\pixtoy.js
 GOTO end
 
 :end
