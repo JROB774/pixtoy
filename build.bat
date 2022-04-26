945591e4f8544ba5abf31e9fe8089f5636ca5187
Joshua Robertson <joshrobertson774@gmail.com>
2022-04-26 09:54:45 +0100

    Update build script to deal with new folder structure.

--- build.bat
+++ build.bat
@@ -1,29 +1,29 @@
 @ECHO off
 
-IF NOT EXIST binary MKDIR binary
+IF NOT EXIST bin MKDIR bin
 
 IF "%~1"=="clean" GOTO clean_dir
 IF "%~1"=="server" GOTO run_server
 IF "%~1"=="setup" GOTO run_setup
 
-IF NOT EXIST binary\lua.o GOTO build_lua
+IF NOT EXIST bin\lua.o GOTO build_lua
 GOTO build_app
 
 :clean_dir
 ECHO cleaning build...
-IF EXIST "binary" DEL binary\*.*?
+IF EXIST "bin" DEL bin\*.*?
 GOTO end
 
 :run_server
 ECHO starting server...
-PUSHD binary
+PUSHD bin
 START python -m http.server
 POPD
 GOTO end
 
 :run_setup
 ECHO setting up emsdk...
-PUSHD emsdk
+PUSHD app\emsdk
 CALL emsdk install latest
 CALL emsdk activate latest
 CALL emsdk_env.bat
@@ -32,20 +32,20 @@ GOTO end
 
 :build_lua
 ECHO building lua library...
-PUSHD lua
-CALL emcc -c -DMAKE_LIB onelua.c -o ..\binary\lua.o
+PUSHD app\lua
+CALL emcc -c -DMAKE_LIB onelua.c -o ..\..\bin\lua.o
 POPD
 GOTO build_app
 
 :build_app
 ECHO building application...
-PUSHD binary
-CALL emcc -c -I ..\lua ..\source\pixtoy.c -o pixtoy.o
-CALL emcc -s WASM=1 -s USE_SDL=2 lua.o pixtoy.o -o pixtoy.js
-COPY ..\source\*.html *.html
-COPY ..\source\*.js *.js
-COPY ..\source\ace\*.js *.js
-COPY ..\source\*.css *.css
+PUSHD bin
+CALL emcc -c -I ..\app\lua ..\app\pixapp.c -o pixapp.o
+CALL emcc -s WASM=1 -s USE_SDL=2 lua.o pixapp.o -o pixtoy.js
+COPY ..\web\*.html *.html
+COPY ..\web\*.js *.js
+COPY ..\web\ace\*.js *.js
+COPY ..\web\*.css *.css
 POPD
 GOTO end
 
