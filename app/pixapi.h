c4cd7e0e50cb5bbf0415214d228079fffe379951
Joshua Robertson <joshrobertson774@gmail.com>
2022-04-28 00:28:09 +0100

    Added basic comments about what the different API calls do.

--- app/pixapi.h
+++ app/pixapi.h
@@ -7,39 +7,39 @@ PIXDEF pixVOID pix_register_api(lua_State* lua);
 PIXDEF pixVOID pix_set_screen(pixU32* screen);
 
 // BITS //////
-PIXAPI(band );
-PIXAPI(bor  );
-PIXAPI(bxor );
-PIXAPI(bnot );
+PIXAPI(band ); // a,b     -> x   :: the bitwise AND operation of a and b
+PIXAPI(bor  ); // a,b     -> x   :: the bitwise OR operation of a and b
+PIXAPI(bxor ); // a,b     -> x   :: the bitwise XOR operation of a and b
+PIXAPI(bnot ); // x       -> x   :: the bitwise NOT operation of a
 // MATH //////
-PIXAPI(abs  );
-PIXAPI(acos );
-PIXAPI(asin );
-PIXAPI(atan );
-PIXAPI(round);
-PIXAPI(ceil );
-PIXAPI(floor);
-PIXAPI(deg  );
-PIXAPI(rad  );
-PIXAPI(exp  );
-PIXAPI(fmod );
-PIXAPI(modf );
-PIXAPI(log  );
-PIXAPI(min  );
-PIXAPI(max  );
-PIXAPI(sqrt );
-PIXAPI(cos  );
-PIXAPI(sin  );
-PIXAPI(tan  );
-PIXAPI(clamp);
-PIXAPI(seed );
-PIXAPI(rand );
+PIXAPI(abs  ); // x       -> x   :: returns absolute value of x
+PIXAPI(acos ); // x       -> x   :: returns arc cosine of x
+PIXAPI(asin ); // x       -> x   :: returns arc sine of x
+PIXAPI(atan ); // y,[x]   -> x   :: returns arc tangent of either  y or y/x
+PIXAPI(round); // x       -> x   :: rounds x to the nearest whole number
+PIXAPI(ceil ); // x       -> x   :: rounds x up to the nearest whole number
+PIXAPI(floor); // x       -> x   :: rounds x down to the nearest while number
+PIXAPI(deg  ); // rad     -> x   :: converts rad from radians to degress
+PIXAPI(rad  ); // deg     -> x   :: converts deg from degrees to radians
+PIXAPI(exp  ); // x       -> x   :: returns base-e exponential of x
+PIXAPI(fmod ); // n,d     -> x   :: returns remainder of n/d
+PIXAPI(modf ); // x       -> i,f :: returns the integral and fractional of x
+PIXAPI(log  ); // x       -> x   :: returns base-e logarithm of x
+PIXAPI(min  ); // a,b     -> x   :: returns the smallest of a and b
+PIXAPI(max  ); // a,b     -> x   :: returns the largest of a and b
+PIXAPI(sqrt ); // x       -> x   :: returns the square root of x
+PIXAPI(cos  ); // x       -> x   :: returns cosine of x
+PIXAPI(sin  ); // x       -> x   :: returns sine of x
+PIXAPI(tan  ); // x       -> x   :: returns tan of x
+PIXAPI(clamp); // x,l,h   -> x   :: returns x clamped between l and h
+PIXAPI(seed ); // [x]     -> x   :: uses current time or x as new random seed
+PIXAPI(rand ); // [x,[y]] -> x   :: generate a random number within a range
 // DRAW //////
-PIXAPI(clrs );
-PIXAPI(pset );
-PIXAPI(pget );
-PIXAPI(line );
-PIXAPI(rect );
-PIXAPI(circ );
+PIXAPI(clrs ); // c              :: clears the screen with color c
+PIXAPI(pset ); // x,y,c          :: sets screen pixel at x,y to color c
+PIXAPI(pget ); // x,y     -> c   :: returns screen pixel color at x,y
+PIXAPI(line ); // x,y,z,w,c      :: draws line from point x,y to z,w
+PIXAPI(rect ); // m,x,y,w,h,c    :: draws a rect with bounds x,y,w,h
+PIXAPI(circ ); // m,x,y,r,t,c    :: draws a circle with radius r at x,y
 
 #endif /* PIXAPI_H__ /////////////////////////////////////////////////////////*/
