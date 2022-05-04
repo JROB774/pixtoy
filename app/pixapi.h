95cec1ee71e2679ca068c3f1d7e5153ad03e6d27
Joshua Robertson <joshrobertson774@gmail.com>
2022-05-04 23:59:42 +0100

    Updated the pixapi header docs to better match the API reference docs.

--- app/pixapi.h
+++ app/pixapi.h
@@ -8,33 +8,33 @@ PIXDEF pixVOID pix_api_register(lua_State* lua);
 // UTIL //////
 PIXAPI(chr  ); // x       -> c   :: converts ascii code x to a character
 // BITS //////
-PIXAPI(band ); // a,b     -> x   :: the bitwise AND operation of a and b
-PIXAPI(bor  ); // a,b     -> x   :: the bitwise OR operation of a and b
-PIXAPI(bxor ); // a,b     -> x   :: the bitwise XOR operation of a and b
-PIXAPI(bnot ); // x       -> x   :: the bitwise NOT operation of a
+PIXAPI(band ); // a,b     -> c   :: the bitwise AND operation of a and b
+PIXAPI(bor  ); // a,b     -> c   :: the bitwise OR operation of a and b
+PIXAPI(bxor ); // a,b     -> c   :: the bitwise XOR operation of a and b
+PIXAPI(bnot ); // x       -> y   :: the bitwise NOT operation of a
 // MATH //////
-PIXAPI(abs  ); // x       -> x   :: returns absolute value of x
-PIXAPI(acos ); // x       -> x   :: returns arc cosine of x
-PIXAPI(asin ); // x       -> x   :: returns arc sine of x
-PIXAPI(atan ); // y,[x]   -> x   :: returns arc tangent of either y or y/x
-PIXAPI(round); // x       -> x   :: rounds x to the nearest whole number
-PIXAPI(ceil ); // x       -> x   :: rounds x up to the nearest whole number
-PIXAPI(floor); // x       -> x   :: rounds x down to the nearest while number
-PIXAPI(deg  ); // rad     -> x   :: converts rad from radians to degress
-PIXAPI(rad  ); // deg     -> x   :: converts deg from degrees to radians
-PIXAPI(exp  ); // x       -> x   :: returns base-e exponential of x
-PIXAPI(fmod ); // n,d     -> x   :: returns remainder of n/d
+PIXAPI(abs  ); // x       -> y   :: returns absolute value of x
+PIXAPI(acos ); // x       -> y   :: returns arc cosine of x
+PIXAPI(asin ); // x       -> y   :: returns arc sine of x
+PIXAPI(atan ); // y,[x]   -> z   :: returns arc tangent of either y or y/x
+PIXAPI(round); // x       -> y   :: rounds x to the nearest whole number
+PIXAPI(ceil ); // x       -> y   :: rounds x up to the nearest whole number
+PIXAPI(floor); // x       -> y   :: rounds x down to the nearest while number
+PIXAPI(deg  ); // r       -> d   :: converts rad from radians to degress
+PIXAPI(rad  ); // d       -> r   :: converts deg from degrees to radians
+PIXAPI(exp  ); // x       -> y   :: returns base-e exponential of x
+PIXAPI(fmod ); // n,d     -> r   :: returns remainder of n/d
 PIXAPI(modf ); // x       -> i,f :: returns the integral and fractional of x
-PIXAPI(log  ); // x       -> x   :: returns base-e logarithm of x
-PIXAPI(min  ); // a,b     -> x   :: returns the smallest of a and b
-PIXAPI(max  ); // a,b     -> x   :: returns the largest of a and b
-PIXAPI(sqrt ); // x       -> x   :: returns the square root of x
-PIXAPI(cos  ); // x       -> x   :: returns cosine of x
-PIXAPI(sin  ); // x       -> x   :: returns sine of x
-PIXAPI(tan  ); // x       -> x   :: returns tan of x
-PIXAPI(clamp); // x,l,h   -> x   :: returns x clamped between l and h
+PIXAPI(log  ); // x       -> y   :: returns base-e logarithm of x
+PIXAPI(min  ); // a,b     -> c   :: returns the smallest of a and b
+PIXAPI(max  ); // a,b     -> c   :: returns the largest of a and b
+PIXAPI(sqrt ); // x       -> y   :: returns the square root of x
+PIXAPI(cos  ); // x       -> y   :: returns cosine of x
+PIXAPI(sin  ); // x       -> y   :: returns sine of x
+PIXAPI(tan  ); // x       -> y   :: returns tan of x
+PIXAPI(clamp); // x,l,h   -> y   :: returns x clamped between l and h
 PIXAPI(seed ); // [x]            :: uses current time or x as new random seed
-PIXAPI(rand ); // [x,[y]] -> x   :: generate a random number within a range
+PIXAPI(rand ); // [x,[y]] -> z   :: generate a random number within a range
 // DRAW //////
 PIXAPI(clrs ); // c              :: clears the screen with color c
 PIXAPI(pset ); // x,y,c          :: sets screen pixel at x,y to color c
